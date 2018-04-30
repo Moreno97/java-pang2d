49c5434da19f7cd442692cd2769fe476c740d910
kerinvel <alfas14@gmail.com>
2018-04-30 12:13:55 +0200

    Añadidos spacio Jframe, y el mapa Jpane

---
+++ src/pang2d/Space.java
@@ -0,0 +1,22 @@
+package pang2d;
+
+import mapanel.Mapanel;
+
+import javax.swing.*;
+
+public class Space extends JFrame {
+    Mapanel p;
+
+    public Space(){
+        this.setBounds(0,0,853,571);
+        this.setTitle("Pang Vrs. DAM");
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        p=new Mapanel();
+        this.getContentPane().add(p);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+    }
+}
